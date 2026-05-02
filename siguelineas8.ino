0573ddb8a73b3034f1fcaf4f413774c573692968
brn2bwild <brn_2_b_wild@hotmail.com>
2026-05-02 09:22:32 -0600

    Modificado el código del PID

--- siguelineas8.ino
+++ siguelineas8.ino
@@ -17,11 +17,8 @@ const int SETPOINT = 3500;
 
 /* Velocidades del siguelíneas */
 const int MAX_SPEED = 70;  // 230 de velocidad funcional
-const int MIN_SPEED = MAX_SPEED * -1;
-const int BRAKE_SPEED = 70;
-
-/* Valor para el diff */
-const int MAX_PWM = 255;
+const int MIN_SPEED = MAX_SPEED * (-1);
+const int BRAKE_SPEED = 255;
 
 /* Cantidad de sensores de la tarjeta */
 const int SENSORS_NUM = 8;
@@ -32,11 +29,18 @@ TB6612 puenteh;
 /* Se declara el arreglo para los sensores */
 uint16_t SenIR[SENSORS_NUM];
 
-int error, last_position, integral, derivative, left_motor_speed, right_motor_speed;
+uint16_t position;
+float error = 0.0, last_error = 0.0;
+float error1 = 0, error2 = 0, error3 = 0, error4 = 0, error5 = 0, error6 = 0;
+float integral = 0.0;
+float derivative = 0.0;
+float pid_output = 0.0;
+
+int left_motor_speed, right_motor_speed;
 
 void setup() {
   pinMode(BUTTON, INPUT);
-  // pinMode(QTR_LEDS, OUTPUT);
+  pinMode(QTR_LEDS, OUTPUT);
   pinMode(LEDS, OUTPUT);
 
   // digitalWrite(QTR_LEDS, HIGH);
@@ -71,53 +75,61 @@ void setup() {
   delay(1000);
 }
 
-void loop() {
-  uint16_t position = qtr.readLineBlack(SenIR);
-
+#ifdef DEBUG
+void printQtrData() {
   /* Esta parte sirve para comprobar que los sensores están leyendo correctamente */
-  // qtr.read(SenIR);
-  // for(int i = 0; i < 8; i++){
-  //   Serial.print(SenIR[i]);
-  //   Serial.print('\t');
-  // }
-  // Serial.println(position);
+  qtr.read(SenIR);
+  for (int i = 0; i < 8; i++) {
+    Serial.print(SenIR[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
+}
+#endif
+
+void computePID() {
+  position = qtr.readLineBlack(SenIR);
 
   error = position - SETPOINT;
 
-/* Estas líneas implementan el freno en las curvas más complicadas*/
-#ifndef DEBUG
-  if (error <= -SETPOINT) {
-    puenteh.motores(-BRAKE_SPEED, BRAKE_SPEED);
-  } else if (error >= SETPOINT) {
-    puenteh.motores(BRAKE_SPEED, -BRAKE_SPEED);
-  }
-#endif
+  error6 = error5;
+  error5 = error4;
+  error4 = error3;
+  error3 = error2;
+  error2 = error1;
+  error1 = error;
+  integral = KI * (error6 + error5 + error4 + error3 + error2 + error1 + error);
 
-  derivative = position - last_position;
+  integral = constrain(integral, MIN_SPEED, MAX_SPEED);
 
-  integral += (KI * error);
-  integral = constrain(integral, -1000, 1000);
+  derivative = KD * (error - last_error);
+  last_error = error;
 
-  int diff = (KP * error) + integral + (KD * derivative);
+  pid_output = (KP * error) + integral + derivative;
+}
 
-  last_position = position;
+void driveMotors() {
+  left_motor_speed = constrain(MAX_SPEED - pid_output, MIN_SPEED, MAX_SPEED);
+  right_motor_speed = constrain(MAX_SPEED + pid_output, MIN_SPEED, MAX_SPEED);
 
-  // left_motor_speed = constrain(MAX_SPEED - diff, MIN_SPEED, MAX_SPEED);
-  // right_motor_speed = constrain(MAX_SPEED + diff, MIN_SPEED, MAX_SPEED);
+  (pid_output > 0) ? puenteh.motores(left_motor_speed, MAX_SPEED) : puenteh.motores(MAX_SPEED, right_motor_speed);
+}
 
-#ifndef DEBUG
-  (diff > 0) ? puenteh.motores(MAX_SPEED - diff, MAX_SPEED) : puenteh.motores(MAX_SPEED, MAX_SPEED + diff);
-#endif
-
-  /* Esta parte sirve para comprobar los valores de las velocidades */
 #ifdef DEBUG
+void printPIDDAta() {
   Serial.print("error: ");
   Serial.print(error);
-  Serial.print(", diff: ");
-  Serial.print(diff);
+  Serial.print(", pid_output: ");
+  Serial.print(pid_output);
   Serial.print(", left speed: ");
-  Serial.print(MAX_SPEED - diff);
+  Serial.print(MAX_SPEED - pid_output);
   Serial.print(", right speed: ");
-  Serial.println(MAX_SPEED + diff);
+  Serial.println(MAX_SPEED + pid_output);
+}
 #endif
+
+void loop() {
+  computePID();
+
+  driveMotors();
 }
